be203f0477bffea505cdd00f046c40f7c320ad68
frckbrice <maebrie2017@gmail.com>
2023-06-07 13:10:12 +0100

    landing page in process

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,28 @@
+import classes from "./AvailableMeals.module.css";
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import {foodInStock} from './GetFoods'
+
+
+const AvailableMeals = () => {
+  const mealsList = foodInStock.map((meal) => (
+    <MealItem
+      key={meal.id}
+      pict={meal.pict}
+      name={meal.name}
+      price={meal.price}
+      id={meal.id}
+      quantity={meal.quantity}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        {mealsList}
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
