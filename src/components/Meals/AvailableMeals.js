bbaa80e303aa2eb55ea451ef6f3d597003184d8d
frckbrice <maebrie2017@gmail.com>
2023-07-10 01:30:15 +0100

    add default food in case not added by admin

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,11 +2,12 @@ import classes from "./AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import { useLocalStorage } from "../../store/useLocalStorage";
+import { foodInStock } from "./GetFoods";
 
 const AvailableMeals = () => {
   const { lsData } = useLocalStorage("displayList");
-
-  const mealsList = lsData.map((meal) => (
+  let mealsList = [];
+lsData ? mealsList = lsData?.map((meal) => (
     <MealItem
       key={meal.id}
       avatar={meal.avatar}
@@ -14,7 +15,16 @@ const AvailableMeals = () => {
       price={meal.price}
       id={meal.id}
     />
-  ));
+  )) : mealsList = foodInStock?.map((meal) => (
+    <MealItem
+      key={meal.id}
+      avatar={meal.pict}
+      name={meal.name}
+      price={meal.price}
+      id={meal.id}
+    />
+  ))
+   
 
   return (
     <section className={classes.meals}>
--- src/components/Meals/GetFoods.jsx
+++ src/components/Meals/GetFoods.jsx
@@ -1,250 +1,250 @@
-// const foodInStock = [
-//   {
-//     id: "1",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Muamba-Galinha.jpg",
-//     name: "Muamba-Galinha ",
-//     description:
-//       "moamba galinha is the Democratic Republic of Congo’s national dish",
-//     price: 22.99,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "2",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Couscous-640x480.jpg",
-//     name: "Couscous North Africa",
-//     description: "Couscous in the Berber language means rolled or rounded",
-//     price: 18.99,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "3",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Jollof-Rice-640x427.jpg",
-//     name: "Jollof Rice",
-//     description: "Jollof-rice A one-pot dish with rice as the star ingredient",
-//     price: 22.99,
-//     nutrients: {
-//         energy: "65 (kcal)",
-//         carbohydrate: "12.9 (g)",
-//         proteine: "2.7 (g)",
-//         fat: "1.2 (g)",
-//         water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "4",
-//     name: "Cachupa",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Cachupa-640x480.jpg",
-//     description:
-//       "Cachupa has a smooth, buttery texture. The taste is an assortment of the different vegetable and meat varieties added to it",
-//     price: 22.99,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "5",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Ndole.jpg",
-//     name: "Cameroon ndole",
-//     description:
-//       "Cameroon ndole is a delicious stew mainly comprising boiled bitter leaves, melon seeds, and peanuts, besides fish or meat",
-//     price: 22.99,
-//     nutrients: {
-//       energy: "64 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "6",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Yassa-640x360.jpg",
-//     name: "yassa or chicken yassa",
-//     description:
-//       "a mouthwatering stew prepared by marinating chicken in vinegar, onion, and lemon.",
-//     price: 6.5,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.8 (g)",
-//       fat: "1.2 (g)",
-//       water: "15.6 (g)",
-//     },
-//   },
-//   {
-//     id: "7",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Kedjenou-640x427.jpg",
-//     name: "Kedjenou",
-//     description:
-//       "it is made by cooking chicken pieces, okra, tomato, eggplant, onion, garlic, thyme, chili pepper, ginger, and chicken broth together.",
+export const foodInStock = [
+  {
+    id: "1",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Muamba-Galinha.jpg",
+    name: "Muamba-Galinha ",
+    description:
+      "moamba galinha is the Democratic Republic of Congo’s national dish",
+    price: 22.99,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "2",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Couscous-640x480.jpg",
+    name: "Couscous North Africa",
+    description: "Couscous in the Berber language means rolled or rounded",
+    price: 18.99,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "3",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Jollof-Rice-640x427.jpg",
+    name: "Jollof Rice",
+    description: "Jollof-rice A one-pot dish with rice as the star ingredient",
+    price: 22.99,
+    nutrients: {
+        energy: "65 (kcal)",
+        carbohydrate: "12.9 (g)",
+        proteine: "2.7 (g)",
+        fat: "1.2 (g)",
+        water: "10.6 (g)",
+    },
+  },
+  {
+    id: "4",
+    name: "Cachupa",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Cachupa-640x480.jpg",
+    description:
+      "Cachupa has a smooth, buttery texture. The taste is an assortment of the different vegetable and meat varieties added to it",
+    price: 22.99,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "5",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Ndole.jpg",
+    name: "Cameroon ndole",
+    description:
+      "Cameroon ndole is a delicious stew mainly comprising boiled bitter leaves, melon seeds, and peanuts, besides fish or meat",
+    price: 22.99,
+    nutrients: {
+      energy: "64 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "6",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Yassa-640x360.jpg",
+    name: "yassa or chicken yassa",
+    description:
+      "a mouthwatering stew prepared by marinating chicken in vinegar, onion, and lemon.",
+    price: 6.5,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.8 (g)",
+      fat: "1.2 (g)",
+      water: "15.6 (g)",
+    },
+  },
+  {
+    id: "7",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Kedjenou-640x427.jpg",
+    name: "Kedjenou",
+    description:
+      "it is made by cooking chicken pieces, okra, tomato, eggplant, onion, garlic, thyme, chili pepper, ginger, and chicken broth together.",
 
-//     price: 7.99,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "8",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Ogbono-Soup.jpg ",
-//     name: "ogbono ",
-//     description:
-//       "made with ogbono seeds alongside other ingredients like onion, red palm oil, seasoning cube, stock, leafy and non-leafy vegetables (like spinach, bitter leaf, okra, tomato) as well as meat or fish",
-//     price: 8.99,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "9",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Chakalaka-640x458.jpg",
-//     name: "chakalaka",
-//     description:
-//       " a spicy vegetable dish prepared with tomato, onion, carrot, bean, bell pepper, ginger, and chili. ",
-//     price: 9.99,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "10",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Boerewors-640x480.jpg    ",
-//     name: "Boerewors f.’ sausage",
-//     description:
-//       "Boer means farmer, and wors translates to sausage, meaning farmers’ sausage, typical of South Africa.",
-//     price: 10.5,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "11",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Bobotie-640x427.jpg",
-//     name: "Bobotie",
-//     description:
-//       "made with lamb or ground beef or occasionally pork. Other ingredients include a whole lot of things like curry powder, spices, herbs, and vinegar.",
+    price: 7.99,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "8",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Ogbono-Soup.jpg ",
+    name: "ogbono ",
+    description:
+      "made with ogbono seeds alongside other ingredients like onion, red palm oil, seasoning cube, stock, leafy and non-leafy vegetables (like spinach, bitter leaf, okra, tomato) as well as meat or fish",
+    price: 8.99,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "9",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Chakalaka-640x458.jpg",
+    name: "chakalaka",
+    description:
+      " a spicy vegetable dish prepared with tomato, onion, carrot, bean, bell pepper, ginger, and chili. ",
+    price: 9.99,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "10",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Boerewors-640x480.jpg    ",
+    name: "Boerewors f.’ sausage",
+    description:
+      "Boer means farmer, and wors translates to sausage, meaning farmers’ sausage, typical of South Africa.",
+    price: 10.5,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "11",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Bobotie-640x427.jpg",
+    name: "Bobotie",
+    description:
+      "made with lamb or ground beef or occasionally pork. Other ingredients include a whole lot of things like curry powder, spices, herbs, and vinegar.",
 
-//     price: 11.99,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "12",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Koshary-640x480.jpg",
-//     name: "Koshary or kosheri",
-//     description:
-//       "is a famous street food that originated in the middle half of the 19th century. This dish’s main ingredients include rice, lentils, and macaroni",
-//     price: 12.99,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "13",
-//     pict: " https://flavorverse.com/wp-content/uploads/2021/01/Alloco-640x416.png",
-//     name: " Alloco",
-//     description:
-//       "a fried plantain dish of West and East Africa, seasoned in garlic, parsley, tomato, onion, green pepper, and black pepper. ",
-//     price: 13.99,
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "2.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "10.6 (g)",
-//     },
-//   },
-//   {
-//     id: "14",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Banga-Soup.jpg",
-//     name: "Banga-Soup",
-//     description:
-//       "It is a delicious Nigerian soup prepared with palm fruit, vegetables, beef, and fish, alongside chili pepper and salt seasonings..",
-//     price: 14.5,
-//     nutrients: {
-//       energy: "41 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "3.9 (g)",
-//       fat: "1.2 (g)",
-//       water: "65.6 (g)",
-//     },
-//   },
-//   {
-//     id: "15",
-//     pict: "https://flavorverse.com/wp-content/uploads/2021/01/Chicken-Tagine-640x575.jpg",
-//     name: "chicken tajine",
-//     description: "Couscous in the Berber language means rolled or rounded",
-//     nutrients: {
-//       energy: "65 (kcal)",
-//       carbohydrate: "18.9 (g)",
-//       proteine: "7.7 (g)",
-//       fat: "0.7 (g)",
-//       water: "10.6 (g)",
-//     },
+    price: 11.99,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "12",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Koshary-640x480.jpg",
+    name: "Koshary or kosheri",
+    description:
+      "is a famous street food that originated in the middle half of the 19th century. This dish’s main ingredients include rice, lentils, and macaroni",
+    price: 12.99,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "13",
+    pict: " https://flavorverse.com/wp-content/uploads/2021/01/Alloco-640x416.png",
+    name: " Alloco",
+    description:
+      "a fried plantain dish of West and East Africa, seasoned in garlic, parsley, tomato, onion, green pepper, and black pepper. ",
+    price: 13.99,
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "2.7 (g)",
+      fat: "1.2 (g)",
+      water: "10.6 (g)",
+    },
+  },
+  {
+    id: "14",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Banga-Soup.jpg",
+    name: "Banga-Soup",
+    description:
+      "It is a delicious Nigerian soup prepared with palm fruit, vegetables, beef, and fish, alongside chili pepper and salt seasonings..",
+    price: 14.5,
+    nutrients: {
+      energy: "41 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "3.9 (g)",
+      fat: "1.2 (g)",
+      water: "65.6 (g)",
+    },
+  },
+  {
+    id: "15",
+    pict: "https://flavorverse.com/wp-content/uploads/2021/01/Chicken-Tagine-640x575.jpg",
+    name: "chicken tajine",
+    description: "Couscous in the Berber language means rolled or rounded",
+    nutrients: {
+      energy: "65 (kcal)",
+      carbohydrate: "18.9 (g)",
+      proteine: "7.7 (g)",
+      fat: "0.7 (g)",
+      water: "10.6 (g)",
+    },
 
-//     price: 15.5,
-//   },
-//   {
-//     id: "16",
-//     pict: " https://flavorverse.com/wp-content/uploads/2021/01/Thieboudienne-640x425.jpg",
-//     name: "thieboudienne",
-//     description:
-//       "Senegal’s national dish, thieboudienne, was prepared by cooking rice and fish in a single pot seasoned in tomato sauce.",
-//     price: 16.99,
-//     nutrients: {
-//       energy: "52 (kcal)",
-//       carbohydrate: "12.9 (g)",
-//       proteine: "4.7 (g)",
-//       fat: "1.2 (g)",
-//       water: "17.6 (g)",
-//     },
-//   },
-// ];
+    price: 15.5,
+  },
+  {
+    id: "16",
+    pict: " https://flavorverse.com/wp-content/uploads/2021/01/Thieboudienne-640x425.jpg",
+    name: "thieboudienne",
+    description:
+      "Senegal’s national dish, thieboudienne, was prepared by cooking rice and fish in a single pot seasoned in tomato sauce.",
+    price: 16.99,
+    nutrients: {
+      energy: "52 (kcal)",
+      carbohydrate: "12.9 (g)",
+      proteine: "4.7 (g)",
+      fat: "1.2 (g)",
+      water: "17.6 (g)",
+    },
+  },
+];
 
 //*function to return a production conditionally
 
 function getfoodInStock(foodInStock, id) {
-  let productData = foodInStock.find((product) => product.id === id);
+  let productData = foodInStock?.find((product) => product.id === id);
 
   if (productData === undefined) {
     return undefined;
